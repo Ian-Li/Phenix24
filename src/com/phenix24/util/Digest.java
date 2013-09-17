2c34ba687f2e61190fc56a0e5085169e127ff4db
Ian-Li <randomlife_@hotmail.com	>
2013-09-17 10:24:49 +0800

    Change-Id: Ia6ddcd3b6848f0b779dfd13ed19e6b6af9e3a365

---
+++ src/com/phenix24/util/Digest.java
@@ -0,0 +1,18 @@
+package com.phenix24.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Digest {
+
+    public static String generate(String algorithm, String message)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        
+        MessageDigest msgDigest = MessageDigest.getInstance(algorithm);
+        msgDigest.update(message.getBytes("UTF-8"));
+        byte[] digest = msgDigest.digest();
+
+        return StringUtils.bytesToHexStr(digest);
+    }
+}
---
+++ src/com/phenix24/util/StringUtils.java
@@ -0,0 +1,33 @@
+package com.phenix24.util;
+
+public class StringUtils {
+
+    public static String bytesToHexStr(byte[] bytes) {
+        if (bytes == null || bytes.length == 0)
+            return null;
+
+        StringBuilder sb = new StringBuilder();
+        int len = bytes.length;
+        for (int i = 0; i < len; i++) {
+            String strTemp = Integer.toHexString(bytes[i] & 0xFF);
+            if (strTemp.length() == 1) {
+                strTemp = "0" + strTemp;
+            }
+            sb.append(strTemp);
+        }
+        return sb.toString().toUpperCase();
+    }
+
+    public static byte[] hexStrToBytes(String hexStr) {
+        if (hexStr == null || hexStr.length() == 0 || hexStr.length() % 2 != 0)
+            return null;
+
+        int len = hexStr.length();
+        byte[] bytes = new byte[len / 2];
+        for (int i = 0; i < len - 1; i += 2) {
+            String strTemp = hexStr.substring(i, i + 2);
+            bytes[i / 2] = (byte) Integer.parseInt(strTemp, 16);
+        }
+        return bytes;
+    }
+}
