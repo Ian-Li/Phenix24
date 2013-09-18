e2873bbfa30ff7653948cec2684d0483d1b359aa
Ian-Li <randomlife_@hotmail.com	>
2013-09-18 15:58:31 +0800

    Change-Id: I35d293a93c6ed8d9d9d22023386d8b7fe42755ca

--- src/com/phenix24/util/Digest.java
+++ src/com/phenix24/util/Digest.java
@@ -8,7 +8,7 @@ public class Digest {
 
     public static String generate(String algorithm, String message)
             throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        
+
         MessageDigest msgDigest = MessageDigest.getInstance(algorithm);
         msgDigest.update(message.getBytes("UTF-8"));
         byte[] digest = msgDigest.digest();
