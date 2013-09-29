b39ee8d33e1a21ad331cf5e5e1292b9e6492564b
Ian-Li <randomlife_@hotmail.com	>
2013-09-29 15:13:44 +0800

    Change-Id: Ib680bae702d52d874720e74e66a4e7d21786d0f6

--- src/com/phenix24/util/Digest.java
+++ src/com/phenix24/util/Digest.java
@@ -1,16 +1,25 @@
 package com.phenix24.util;
 
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Digest {
 
+    /**
+     * Generate message digest.
+     * 
+     * @param algorithm
+     *            the name of the algorithm to use,e.g."SHA-1"
+     * @param message
+     * @return message digest
+     * @throws NoSuchAlgorithmException
+     *             if the specified algorithm is not available
+     */
     public static String generate(String algorithm, String message)
-            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+            throws NoSuchAlgorithmException {
 
         MessageDigest msgDigest = MessageDigest.getInstance(algorithm);
-        msgDigest.update(message.getBytes("UTF-8"));
+        msgDigest.update(message.getBytes());
         byte[] digest = msgDigest.digest();
 
         return StringUtils.bytesToHexStr(digest);
