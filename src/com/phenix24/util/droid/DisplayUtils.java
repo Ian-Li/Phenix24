197b29a07c285829a615b7c8790a6c7be7426c58
Ian-Li <randomlife_@hotmail.com	>
2013-09-29 15:22:31 +0800

    Change-Id: Idbe17a04e8bed05a1e42d0ea26bc0d24957cd0ae

--- src/com/phenix24/util/droid/DisplayUtils.java
+++ src/com/phenix24/util/droid/DisplayUtils.java
@@ -7,21 +7,45 @@ import android.view.WindowManager;
 
 public class DisplayUtils {
 
+    /**
+     * Get the absolute width of the display in pixels.
+     * 
+     * @param context
+     * @return width in pixels
+     */
     public static int getWidth(Context context) {
         DisplayMetrics displayMetr = getDisplayMetrics(context);
         return displayMetr.widthPixels;
     }
 
+    /**
+     * Get the absolute height of the display in pixels.
+     * 
+     * @param context
+     * @return height in pixels
+     */
     public static int getHeight(Context context) {
         DisplayMetrics displayMetr = getDisplayMetrics(context);
         return displayMetr.heightPixels;
     }
 
+    /**
+     * Get the logical density of the display.
+     * 
+     * @param context
+     * @return density of the display
+     */
     public static float getDensity(Context context) {
         DisplayMetrics displayMetr = getDisplayMetrics(context);
         return displayMetr.density;
     }
 
+    /**
+     * Get the screen density expressed as dots-per-inch
+     * 
+     * @param context
+     * @return the screen density
+     */
     public static int getDensityDPI(Context context) {
         DisplayMetrics displayMetr = getDisplayMetrics(context);
         return displayMetr.densityDpi;
