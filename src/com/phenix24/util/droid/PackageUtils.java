235647ce3dcfb9e494fc38407960e044784d6b7e
Ian-Li <randomlife_@hotmail.com	>
2013-10-09 17:06:05 +0800

    Change-Id: I081685e9448039b46fe6d93d93680238cf0be404

--- src/com/phenix24/util/droid/PackageUtils.java
+++ src/com/phenix24/util/droid/PackageUtils.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
+import android.text.TextUtils;
 
 public class PackageUtils {
 
@@ -46,16 +47,16 @@ public class PackageUtils {
      * AndroidManifest.xml
      * 
      * @param context
-     * @param pkaName
-     *            android package name
+     * @param pkgName
+     *            application package name
      * @return version code
      * @throws NameNotFoundException
      *             if app not installed
      */
-    public static int getAppVersionCode(Context context, String pkaName)
+    public static int getAppVersionCode(Context context, String pkgName)
             throws NameNotFoundException {
         PackageManager pkgManager = context.getPackageManager();
-        PackageInfo pkgInfo = pkgManager.getPackageInfo(pkaName, 0);
+        PackageInfo pkgInfo = pkgManager.getPackageInfo(pkgName, 0);
         return pkgInfo.versionCode;
     }
 
@@ -64,17 +65,38 @@ public class PackageUtils {
      * AndroidManifest.xml.
      * 
      * @param context
-     * @param pkaName
-     *            android package name
+     * @param pkgName
+     *            application package name
      * @return version name
      * @throws NameNotFoundException
      *             if app not installed
      */
-    public static String getAppVersionName(Context context, String pkaName)
+    public static String getAppVersionName(Context context, String pkgName)
             throws NameNotFoundException {
         PackageManager pkgManager = context.getPackageManager();
-        PackageInfo pkgInfo = pkgManager.getPackageInfo(pkaName, 0);
+        PackageInfo pkgInfo = pkgManager.getPackageInfo(pkgName, 0);
         return pkgInfo.versionName;
     }
 
+    /**
+     * Check whether the application package is uninstalled
+     * 
+     * @param context
+     * @param pkgName
+     *            application package name
+     * @return false if the application uninstalled,otherwise return true
+     */
+    public static boolean isPackageExist(Context context, String pkgName) {
+        if (TextUtils.isEmpty(pkgName))
+            return false;
+
+        boolean isExist = true;
+        try {
+            PackageManager pkgManager = context.getPackageManager();
+            pkgManager.getPackageInfo(pkgName, 0);
+        } catch (NameNotFoundException e) {
+            isExist = false;
+        }
+        return isExist;
+    }
 }
