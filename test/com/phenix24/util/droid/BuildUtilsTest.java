529de91f7e862c70010fa7848d63857a2b11bc1f
Ian-Li <randomlife_@hotmail.com	>
2013-10-16 17:46:46 +0800

    Change-Id: I46eecaa24d124f2b1496a8d0c55575efc6b0132f

--- test/com/phenix24/util/droid/BuildUtilsTest.java
+++ test/com/phenix24/util/droid/BuildUtilsTest.java
@@ -1,5 +1,6 @@
 package com.phenix24.util.droid;
 
+import android.os.Build;
 import android.test.InstrumentationTestCase;
 import android.util.Log;
 
@@ -10,6 +11,25 @@ public class BuildUtilsTest extends InstrumentationTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+    }
+
+    public void test_build() {
+        Log.i(TAG, "----test_build----");
+        String build = "BOARD:" + Build.BOARD + "\n" + "BOOTLOADER:" + Build.BOOTLOADER
+                + "\n" + "BRAND:" + Build.BRAND + "\n" + "CPU_ABI:" + Build.CPU_ABI
+                + "\n" + "CPU_ABI2:" + Build.CPU_ABI2 + "\n" + "DEVICE:" + Build.DEVICE
+                + "\n" + "DISPLAY:" + Build.DEVICE + "\n" + "FINGERPRINT:"
+                + Build.FINGERPRINT + "\n" + "HARDWARE:" + Build.HARDWARE + "\n"
+                + "HOST:" + Build.HOST + "\n" + "ID:" + Build.ID + "\n" + "MANUFACTURER:"
+                + Build.MANUFACTURER + "\n" + "MODEL:" + Build.MODEL + "\n" + "PRODUCT:"
+                + Build.PRODUCT + "\n" + "RADIO:" + Build.RADIO + "\n" + "SERIAL:"
+                + Build.SERIAL + "\n" + "TAGS:" + Build.TAGS + "\n" + "TIME:"
+                + Build.TIME + "\n" + "TYPE:" + Build.TYPE + "\n" + "USER:" + Build.USER
+                + "\n" + "Build.VERSION.CODENAME:" + Build.VERSION.CODENAME + "\n"
+                + "Build.VERSION.INCREMENTAL:" + Build.VERSION.INCREMENTAL + "\n"
+                + "Build.VERSION.RELEASE:" + Build.VERSION.RELEASE + "\n"
+                + "Build.VERSION.SDK_INT:" + Build.VERSION.SDK_INT;
+        Log.i(TAG, build);
     }
 
     public void test_getFingerprint() {
@@ -34,5 +54,17 @@ public class BuildUtilsTest extends InstrumentationTestCase {
         Log.i(TAG, "----test_getSerial----");
         String serial = BuildUtils.getSerial();
         Log.i(TAG, "serial:" + serial);
+    }
+
+    public void test_getVersionRelease() {
+        Log.i(TAG, "----test_getVersionRelease----");
+        String versionRelease = BuildUtils.getVersionRelease();
+        Log.i(TAG, "versionRelease:" + versionRelease);
+    }
+
+    public void test_getVersionSDK() {
+        Log.i(TAG, "----getVersionSDK----");
+        int versionSDK = BuildUtils.getVersionSDK();
+        Log.i(TAG, "versionSDK:" + versionSDK);
     }
 }
