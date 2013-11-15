a5533b4725cdf98864078f1b57faf38ccff20317
Ian-Li <randomlife_@hotmail.com	>
2013-11-15 13:17:32 +0800

    Change-Id: Id8abedc0d00f6ad381a9b6ce296c2563fc209bed

--- src/com/phenix24/http/HttpException.java
+++ src/com/phenix24/http/HttpException.java
@@ -1,6 +1,5 @@
 package com.phenix24.http;
 
-import java.io.IOException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
@@ -9,31 +8,38 @@ public class HttpException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    //
-    // exception category
-    //
-    public static final int UNKNOWN = 0;
-    public static final int NETWORK_UNAVAILABLE = 1;
-    public static final int UNKNOWN_HOST = 2;
-    public static final int SOCKET = 3;
-    public static final int SOCKET_TIMEOUT = 4;
-    public static final int IO = 5;
+    /** Unknow exception when perform http task */
+    public static final int UNKNOWN = 0x00;
+    /** Network unavailable */
+    public static final int NETWORK_UNAVAILABLE = 0x01;
+    /** Host unavailable */
+    public static final int UNKNOWN_HOST = 0x02;
+    /** Socket cant be initialized */
+    public static final int SOCKET = 0x03;
+    /** Socket read timeout */
+    public static final int SOCKET_TIMEOUT = 0x04;
+    /** Http status code error */
+    public static final int STATUS_CODE_ERROR = 0x05;
 
     private int code;
 
-    public HttpException() {
-        super();
-        this.code = UNKNOWN;
+    public int getCode() {
+        return code;
     }
 
-    public HttpException(int code) {
-        super("HTTP Exception[Code:" + code + "]");
-        this.code = code;
+    public HttpException() {
+        super("Http exception[unknown]");
+        this.code = UNKNOWN;
     }
 
     public HttpException(String message) {
         super(message);
         code = UNKNOWN;
+    }
+
+    public HttpException(int code) {
+        super("Http exception[code:" + code + "]");
+        this.code = code;
     }
 
     public HttpException(int code, String msg) {
@@ -45,12 +51,12 @@ public class HttpException extends Exception {
         super(cause);
         if (cause instanceof UnknownHostException) {
             code = UNKNOWN_HOST;
-        } else if (cause instanceof SocketTimeoutException) {
-            code = SOCKET_TIMEOUT;
         } else if (cause instanceof SocketException) {
             code = SOCKET;
-        } else if (cause instanceof IOException) {
-            code = IO;
+        } else if (cause instanceof SocketTimeoutException) {
+            code = SOCKET_TIMEOUT;
+        } else if (cause instanceof HttpException) {
+            code = ((HttpException) cause).getCode();
         } else {
             code = UNKNOWN;
         }
@@ -60,23 +66,14 @@ public class HttpException extends Exception {
         super(message, cause);
         if (cause instanceof UnknownHostException) {
             code = UNKNOWN_HOST;
-        } else if (cause instanceof SocketTimeoutException) {
-            code = SOCKET_TIMEOUT;
         } else if (cause instanceof SocketException) {
             code = SOCKET;
-        } else if (cause instanceof IOException) {
-            code = IO;
+        } else if (cause instanceof SocketTimeoutException) {
+            code = SOCKET_TIMEOUT;
+        } else if (cause instanceof HttpException) {
+            code = ((HttpException) cause).getCode();
         } else {
             code = UNKNOWN;
         }
     }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
 }
